226833358cc3e1cc50aa2ea7ad3c2adf54708ef0
Igor Puchkov <sibmost1@ngs.ru>
2022-12-17 13:22:34 +0700

    task2

--- task_2.py
+++ task_2.py
@@ -8,3 +8,14 @@
 4
 [1, 2, 6, 24]
 """
+
+from itertools import accumulate
+import operator
+
+N = int(input('Введите число: '))
+
+
+def get_prods(N):
+    return list(accumulate([x for x in range(1, N + 1)], operator.mul))
+
+print(get_prods(N))
