b0081fb436280b636ad8836fa36b37ed3d9c9b6f
Igor Puchkov <sibmost1@ngs.ru>
2022-12-17 13:24:24 +0700

    task3

--- task_3.py
+++ task_3.py
@@ -16,3 +16,9 @@
 3
 6.62
 """
+
+
+print('Введите число')
+n = int(input())
+lst = [round((1 + 1 / i) ** i, 3) for i in range(1, n + 1)]
+print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 3)}')
